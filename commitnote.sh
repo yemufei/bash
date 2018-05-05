be475109f2c92cdacdbd72b861cbc767e57a170f
yemufei <yemufei@126.com>
2018-05-05 13:10:01 +0800

    default

--- commitnote.sh
+++ commitnote.sh
@@ -3,10 +3,13 @@
 #mac dir
 #cd /Users/yemufei/Desktop/DEV/GitHub/iNotes
 #ubuntu dir
-
 githubdir="/home/yemufei/github/"
+#echo "hello,I'm running" >> /home/yemufei/tmplog/11.log
 cd "${githubdir}iNotes"
-git push
+git add .
+git commit -m 'default'
 
 cd "${githubdir}bash"
-git push
+git add .
+git commit -m 'default'
+
