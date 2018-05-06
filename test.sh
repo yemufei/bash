222a5c7fc75ba10df56ab85ccfbefcb309e406b5
wb-ymf342847 <wb-ymf342847@alibaba-inc.com>
2018-05-06 11:33:25 +0800

    default

--- test.sh
+++ test.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
 #echo '星期'`date +%w`
-getChineseWeekday(){
-weekday=1
-chineseWeekday=" ";
-case ${weekday} in
-  1) ${chineseWeekday}="星期一"
-;;
-esac
-return ${chineseWeekday}
-}
- getChineseWeekday
-
-echo "$?"
+echo "hello"
