5c7e9c14b70bc0d822ff5931f659c4793cb8daf8
yemufei <yemufei@126.com>
2018-05-09 23:20:10 +0800

    default

--- autochangewallpapers.sh
+++ autochangewallpapers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "hahah"
 
-files= `ls -U /usr/share/backgrounds | grep -v contest`
+files=`ls -U /usr/share/backgrounds | grep -v contest`
 last_file="empty"
 
 echo '<background>'
@@ -14,7 +14,7 @@ echo ' <minute>00</minute>'
 echo ' <second>00</second>'
 echo ' </starttime>'
 
-for current_file in $files
+for current_file in ${files}
 do
   echo "$current_file"
 #fi
