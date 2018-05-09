2bf067dc342ec68664a255291b1bb3efca54d601
yemufei <yemufei@126.com>
2018-05-09 23:15:07 +0800

    default

--- autochangewallpapers.sh
+++ autochangewallpapers.sh
@@ -16,24 +16,7 @@ echo ' </starttime>'
 
 for current_file in $files
 do
-  if [[ lastfile=="empty" ]]
-then
- lastfile=$current_file
-    echo ' <static>'
-    echo ' <duration>300.0</duration>'
-    echo " <file>/usr/share/backgrounds/$lastfile</file>"
-    echo '</static>'
-else
-    echo  '<transition>'
-    echo  '<duration>5.0</duration>'
-    echo " <from>/usr/share/backgrounds/$last_file</from>"
-    echo " <to>/usr/share/backgrounds/$currentfile</to>"
-    echo '</transition>'
-    echo '<static>'
-    echo '<duration>300.0</duration>'
-    echo "<file>/usr/share/backgrounds/$current_file</file>"
-    echo ' </static>'
-    last_file=$current_file
-fi
+  echo "$current_file"
+#fi
 done
 echo '</background>'
